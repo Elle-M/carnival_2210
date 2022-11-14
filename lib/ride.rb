43e247191970586030ac013813bf6143ef75d446
Elle Majors <hellolovely@ellemajors>
2022-11-14 11:58:26 -0700

    feat: Add ride class

--- lib/ride.rb
+++ lib/ride.rb
@@ -1,3 +1,25 @@
 class Ride
+  attr_reader :name,
+              :min_height,
+              :admission_fee,
+              :excitement,
+              :rider_log,
+              :total_revenue
 
+  def initialize(attributes)
+    @name = attributes[:name]
+    @min_height = attributes[:min_height]
+    @admission_fee = attributes[:admission_fee]
+    @excitement = attributes[:excitement]
+    @rider_log = {}
+    @total_revenue = 0
+  end
+
+  def board_rider(visitor)
+    @rider_log + visitor
+  end
+
+  def spending_money
+    @total_revenue = @spending_money - @admission_fee
+  end
 end
